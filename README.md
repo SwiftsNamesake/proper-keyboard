71cab72411dbd74dd0adc75a24890d1515e6d591
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2017-02-15 00:41:34 +0100

    Added some missing keys and a new function

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-This package introduces proper type safe keys, to save you - humble developer - from having to look up
+# Proper Keyboard
+This Elm package introduces proper type safe keys, to save you - humble developer - from having to look up
 or (God forbid) _memorise_ JavaScript keycodes.
+
+Since this is an open source project, contributions and feedback are welcome.
+
+# Usage
+
+# TODO
+- [ ] Complete the documentation
+- [ ] Tests
+- [ ] Key descriptions (although how do we deal with international layouts)
+- [ ] Full coverage (all keys must be representable)
+- [ ] Key categories (Numeric, operator, punctuation, letter), possibly intersecting
+- [ ] Physical layout descriptions
--- src/Keyboard/Key.elm
+++ src/Keyboard/Key.elm
@@ -2,13 +2,13 @@
 -- API -------------------------------------------------------------------------------------------------------------------------------------
 
 module Keyboard.Key exposing (..)
-{-| Readable and typesafe key names
+{-| Readable and typesafe key names.
 
 # Definition
 @docs Key, Side
 
 #
-@docs fromCode, code
+@docs fromCode, code, toChar
 
 -}
 
@@ -18,28 +18,53 @@ import Keyboard exposing (KeyCode)
 
 -- Types -----------------------------------------------------------------------------------------------------------------------------------
 
-{-| Represents a Key -}
--- TODO | - Complete the set
+{-| Represents a Key. Currently incomplete. -}
+-- TODO | - Complete the set (Function keys, Numpad, etc.)
 --        - Categories and groups (eg. NumPad Int | Letter Char)
 --        - Distinguishing duplicates (LeftCtrl | RightCtrl | LeftAlt | RightAlt)
 --        - Should there be an AltLeft/AltRight?
 --        - How do we deal with AltGr?
 --        - How do we deal with Numeric?
 --        - Sort out the logical grouping
-type Key =   A | B | C | D | E | F | G | H | I | J
-           | K | L | M | N | O | P | Q | R | S | T
-           | U | V | W | X | Y | Z
-           | Left | Right | Up | Down
-           | Shift (Maybe Side) | Ctrl (Maybe Side) | Alt
-           | Tab | CapsLock | Escape
-           | Enter | Backspace
-           | Delete | PageUp | PageDown | End | Home
-           | Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Nine
-           | Insert | PrintScreen | PauseBreak
-           | Windows | Command | ChromeSearch
-           | NumLock
-           | Ambiguous (List Key) -- TODO: Pattern synonyms?
-           | Unknown KeyCode -- | Special _
+type Key =
+  -- Letters
+    A | B | C | D | E | F | G | H | I | J
+  | K | L | M | N | O | P | Q | R | S | T
+  | U | V | W | X | Y | Z
+
+  -- Arrow keys
+  | Left | Right | Up | Down
+
+  -- Modifiers
+  | Shift (Maybe Side)
+  | Ctrl  (Maybe Side)
+  | Alt | Tab | CapsLock
+  
+  -- Control 
+  | Spacebar | Escape | Enter | Backspace | Delete | PageUp | PageDown | End | Home
+
+  -- Digits row
+  | Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Nine
+
+  | Insert | PrintScreen | PauseBreak
+  | Windows | Command | ChromeSearch
+  | NumLock | ScrollLock
+
+  -- Function keys
+  | F1 | F2 | F3 | F4 | F5 | F6 | F7 | F8 | F9 | F10 | F11 | F12
+
+  -- Numpad
+  | NumpadZero | NumpadOne | NumpadTwo | NumpadThree | NumpadFour | NumpadFive | NumpadSix | NumpadSeven | NumpadEight | NumpadNine
+  | Multiply | Add | Subtract | Decimal | Divide
+
+  -- Unspecified
+  | Ambiguous (List Key) -- TODO: Pattern synonyms?
+  | Unknown KeyCode -- | Special _
+
+
+-- {-| Represents a Key category -}
+-- TODO | - I have to think carefully about how to define categories
+-- type Category = Character Char | Modifier ModifierKey
 
 
 {-| Type used to distinguish between multiple instances of the same key (such as Left Ctrl and Right Ctrl) -}
@@ -53,6 +78,7 @@ fromCode : KeyCode -> Key
 fromCode code = case code of
 
   --
+  8 -> Backspace
   9 -> Tab
 
   --
@@ -130,6 +156,43 @@ fromCode code = case code of
 
   -- System
   91 -> Ambiguous [Windows, Command, ChromeSearch]
+  
+  -- Numpad
+  96  -> NumpadZero
+  97  -> NumpadOne
+  98  -> NumpadTwo
+  99  -> NumpadThree
+  100 -> NumpadFour
+  101 -> NumpadFive
+  102 -> NumpadSix
+  103 -> NumpadSeven
+  104 -> NumpadEight
+  105 -> NumpadNine
+  
+  106 -> Multiply
+  107 -> Add
+  -- TODO: Numpad Enter
+  109 -> Subtract
+  110 -> Decimal
+  111 -> Divide
+
+  -- Function keys
+  112 -> F1
+  113 -> F2
+  114 -> F3
+  115 -> F4
+  116 -> F5
+  117 -> F6
+  118 -> F7
+  119 -> F8
+  120 -> F9
+  121 -> F10
+  122 -> F11
+  123 -> F12
+
+  -- Locking keys
+  144 -> NumLock
+  145 -> ScrollLock
 
   -- Miscellaneous
   _ -> Unknown code
@@ -141,7 +204,8 @@ code : Key -> Maybe KeyCode
 code key = case key of
 
   --
-  Tab -> Just 9
+  Spacebar -> Just 8
+  Tab      -> Just 9
 
   --
   Enter -> Just 13
@@ -159,7 +223,7 @@ code key = case key of
   --
   Backspace -> Just 32
 
-  --
+  -- Navigation
   PageUp   -> Just 33
   PageDown -> Just 34
   End      -> Just 35
@@ -222,10 +286,115 @@ code key = case key of
   Command -> Just 91
   ChromeSearch -> Just 91
 
-  --
-  NumLock -> Just 144
+  -- Numpad digits
+  NumpadZero  -> Just  96
+  NumpadOne   -> Just  97
+  NumpadTwo   -> Just  98
+  NumpadThree -> Just  99
+  NumpadFour  -> Just 100
+  NumpadFive  -> Just 101
+  NumpadSix   -> Just 102
+  NumpadSeven -> Just 103
+  NumpadEight -> Just 104
+  NumpadNine  -> Just 105
+  
+  -- Numpad operators
+  Multiply -> Just 106
+  Add      -> Just 107
+  -- TODO: NumpadEnter here?
+  Subtract -> Just 109
+  Decimal  -> Just 110
+  Divide   -> Just 111
+
+  -- Function keys
+  F1  -> Just 112
+  F2  -> Just 113
+  F3  -> Just 114
+  F4  -> Just 115
+  F5  -> Just 116
+  F6  -> Just 117
+  F7  -> Just 118
+  F8  -> Just 119
+  F9  -> Just 120
+  F10 -> Just 121
+  F11 -> Just 122
+  F12 -> Just 123
+
+  -- Locking keys
+  NumLock    -> Just 144
+  ScrollLock -> Just 145 
 
   -- Miscellaneous
   Unknown _ -> Nothing
+
+
+{-| Attempt to transform a Key into a Char. This does not work for 'special' keys that are not used to type symbols. -}
+toChar : Key -> Maybe Char
+toChar key = case key of
+  
+  -- Space
+  Spacebar -> Just ' '
+
+  -- Digits
+  Zero  -> Just '0'
+  One   -> Just '1'
+  Two   -> Just '2'
+  Three -> Just '3'
+  Four  -> Just '4'
+  Five  -> Just '5'
+  Six   -> Just '6'
+  Seven -> Just '7'
+  Eight -> Just '8'
+  Nine  -> Just '9'
+
+  -- Letters
+  A -> Just 'A'
+  B -> Just 'B'
+  C -> Just 'C'
+  D -> Just 'D'
+  E -> Just 'E'
+  F -> Just 'F'
+  G -> Just 'G'
+  H -> Just 'H'
+  I -> Just 'I'
+  J -> Just 'J'
+  K -> Just 'K'
+  L -> Just 'L'
+  M -> Just 'M'
+  N -> Just 'N'
+  O -> Just 'O'
+  P -> Just 'P'
+  Q -> Just 'Q'
+  R -> Just 'R'
+  S -> Just 'S'
+  T -> Just 'T'
+  U -> Just 'U'
+  V -> Just 'V'
+  W -> Just 'W'
+  X -> Just 'X'
+  Y -> Just 'Y'
+  Z -> Just 'Z'
+
+  -- Numpad digits
+  NumpadZero  -> Just '0'
+  NumpadOne   -> Just '1'
+  NumpadTwo   -> Just '2'
+  NumpadThree -> Just '3'
+  NumpadFour  -> Just '4'
+  NumpadFive  -> Just '5'
+  NumpadSix   -> Just '6'
+  NumpadSeven -> Just '7'
+  NumpadEight -> Just '8'
+  NumpadNine  -> Just '9'
+  
+  -- Numpad operators
+  Multiply -> Just '*'
+  Add      -> Just '+'
+  Subtract -> Just '-'
+  Divide   -> Just '/'
+  
+  -- Dunno
+  _ -> Nothing
+
 
 -- Tests -----------------------------------------------------------------------------------------------------------------------------------
